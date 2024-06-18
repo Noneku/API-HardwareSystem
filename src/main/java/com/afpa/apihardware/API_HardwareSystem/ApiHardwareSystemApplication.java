93c6004876061682a6064ff55dcd6606d4cc378a
None.ku <dev.nassim.pro@gmail.com>
2024-06-18 13:37:46 +0200

    First commit

---
+++ src/main/java/com/afpa/apihardware/API_HardwareSystem/ApiHardwareSystemApplication.java
@@ -0,0 +1,13 @@
+package com.afpa.apihardware.API_HardwareSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHardwareSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHardwareSystemApplication.class, args);
+	}
+
+}
